0b5c580cab0031041a8b259d61b2639c1b2cbf15
a-HA7-z <hosseinazizpour76@gmail.com>
2025-04-18 20:42:41 +0330

    revert 'Second Commit'

--- main.cpp
+++ main.cpp
@@ -1,13 +1,9 @@
 #include <iostream>
 using namespace std;
 
-void stuNum(){
-    cout << "4033613057" << endl;
-}
+
 
 int main() {
     cout << "Hello, World!" << endl;
-
-    stuNum();
     return 0;
 }
