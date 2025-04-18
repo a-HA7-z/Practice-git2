d83530511b2a502d8f93115656239826453e2164
a-HA7-z <hosseinazizpour76@gmail.com>
2025-04-18 19:53:54 +0330

    Second Commit

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 using namespace std;
 
-
+void stuNum(){
+    cout << "4033613057" << endl;
+}
 
 int main() {
     cout << "Hello, World!" << endl;
+
+    stuNum();
     return 0;
 }
